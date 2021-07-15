41698ef8f64f03b8cbd60f6b279ce4d4bc958177
dikiogres <dikiogres@gmail.com>
2021-07-15 20:27:45 +0700

    informobile

---
+++ app/src/main/java/com/example/informobile_201/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.informobile_201
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
